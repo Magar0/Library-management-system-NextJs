48b95269990846b1f655b92abe42d7b173f28a75
Magar0 <rakeshmagarr0@gmail.com>
2025-03-05 10:50:12 +0545

    fixed bug

--- database/drizzle.ts
+++ database/drizzle.ts
@@ -5,5 +5,3 @@ import { neon } from "@neondatabase/serverless";
 
 const sql = neon(config.env.databaseUrl);
 export const db = drizzle({ client: sql });
-
-const result = await db.execute("select 1");
